44c01de8316bdb93191bae45a1943692646d15fc
Tomicyo <dsotsen@gmail.com>
2022-06-07 16:16:01 +0800

    IrisBuild 2022 Update, Alpha version

--- external/libirisbuild/libirisbuild.cmake
+++ external/libirisbuild/libirisbuild.cmake
@@ -1,6 +1,7 @@
 get_filename_component(install_prefix "${CMAKE_CURRENT_LIST_DIR}" ABSOLUTE)
 
 add_library(ext::ibvm SHARED IMPORTED)
+add_library(ext::irisvfs SHARED IMPORTED)
 
 if(WIN32)
     set_target_properties(ext::ibvm PROPERTIES 
@@ -10,4 +11,12 @@ if(WIN32)
     )
     set(ibvm_DLL "${install_prefix}/bin/ibvm.dll")
     set(cldump_DLL "${install_prefix}/bin/cl_inc_dump.dll")
+
+    set_target_properties(ext::irisvfs PROPERTIES 
+        IMPORTED_LOCATION "${install_prefix}/bin/irisvfs2um.dll"
+        IMPORTED_IMPLIB "${install_prefix}/lib/irisvfs2um.lib"
+        INTERFACE_INCLUDE_DIRECTORIES "${install_prefix}/include"
+    )
+    set(irisvfs_DLL "${install_prefix}/bin/irisvfs2um.dll")
+    set(irisvfs_DRIVER "${install_prefix}/bin/irisvfs2km.sys")
 endif()
